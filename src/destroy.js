6c7f1365712b43164490826a027c8b5bee160bcb
Jason Mulligan <jason.mulligan@avoidwork.com>
2012-10-29 19:32:13 -0400

    Tweaking destroy()

--- src/destroy.js
+++ src/destroy.js
@@ -15,14 +15,15 @@ var destroy = function (obj, arg) {
 		var root = false,
 		    li, section;
 
-		i = i.toLowerCase().replace(/^\/{1,1}/, "");
-		i = "/" + i;
+		i  = i.toLowerCase().replace(/^\/{1,1}/, "");
+		i  = "/" + i;
+		li = obj.find("a[data-route=\"" + i + "\"]")[0].parentNode;
 
-		li      = obj.find("a[data-route=\"" + i + "\"]")[0].parentNode;
-		section = li.parentNode.parentNode.find("section[data-hash=\"" + i + "\"]")[0];
-
-		li.destroy();
-		section.destroy();
+		if (typeof li !== "undefined") {
+			section = li.parentNode.parentNode.find("section[data-hash=\"" + i + "\"]")[0];
+			section.destroy();
+			li.destroy();
+		}
 
 		// Loading initial route if the current one was just destroyed
 		if ($.route.hash() === i) $.route.hash($.route.initial);
